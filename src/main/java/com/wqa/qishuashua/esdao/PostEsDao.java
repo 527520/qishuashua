9466d208a17296ac50c6eea5a794ec7a8d81ae88
wuqian <2194961543@qq.com>
2024-08-24 22:46:45 +0800

    项目初始化

---
+++ src/main/java/com/wqa/qishuashua/esdao/PostEsDao.java
@@ -0,0 +1,14 @@
+package com.wqa.qishuashua.esdao;
+
+import com.wqa.qishuashua.model.dto.post.PostEsDTO;
+
+import java.util.List;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * 帖子 ES 操作
+ */
+public interface PostEsDao extends ElasticsearchRepository<PostEsDTO, Long> {
+
+    List<PostEsDTO> findByUserId(Long userId);
+}
---
+++ src/main/java/com/wqa/qishuashua/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.wqa.qishuashua.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.wqa.qishuashua.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
